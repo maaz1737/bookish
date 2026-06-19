0bd33ea84adcee4befbd9e57d87153dd80cfbb7c
maaz <khalidmaaz766@gmail.com>
2026-06-19 20:25:38 +0500

    home blade design fixes , added dynamic category , addes news letter design

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,8 @@ Route::prefix('admin')->name('admin.')
 
         // Module 2: Categories
         Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
+        Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
+
         Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
         Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
         Route::put('categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
