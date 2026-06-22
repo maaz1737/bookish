c99d66acc2f99ccf1fa03f0526b56c648fc27949
maaz <khalidmaaz766@gmail.com>
2026-06-23 03:33:35 +0500

    merged code

--- app/Http/Controllers/Storefront/HomeController.php
+++ app/Http/Controllers/Storefront/HomeController.php
@@ -12,8 +12,6 @@ class HomeController extends Controller
     public function index()
     {
 
-
-
         return view('storefront.home', [
             'heroBanners' => \App\Models\Banner::where('is_active', true)->orderBy('order', 'asc')->get(),
             'schools'    => School::where('is_active', true)->get(),
