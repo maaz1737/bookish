deffce6818243b1dc3a9034b0f5157c3334ddeba
Qamar Rafahan <qamarrafhan@gmail.com>
2026-07-17 00:58:00 +0500

    A-ui fix

--- resources/views/partials/category-card.blade.php
+++ resources/views/partials/category-card.blade.php
@@ -5,7 +5,7 @@
                 class="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500" />
         </div>
         <div class="py-3 pt-5">
-            <h3 class="text-base font-bold text-[#001F54] px-4 filter-name hover:text-blue-700 hover:underline">
+            <h3 class="text-base font-bold text-[#001F54] group-hover:text-[#ff7a00] hover:text-[#ff7a00] px-4 filter-name transition-colors cursor-pointer">
                 {{ $category->name }}
             </h3>
         </div>
