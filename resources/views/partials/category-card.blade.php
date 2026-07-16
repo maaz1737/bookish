e7e2b13cc7f0b599f8c679368a29daab84ac39e9
maaz <khalidmaaz766@gmail.com>
2026-07-17 01:26:28 +0500

    changes in the ui

--- resources/views/partials/category-card.blade.php
+++ resources/views/partials/category-card.blade.php
@@ -8,15 +8,15 @@
         <div class="py-3 pt-5">
             <h3
                 class="text-sm font-bold text-[#0a1a3d] hover:text-[#1e3a8a] transition-colors leading-snug line-clamp-2 filter-name  px-4">
-                {{ ucfirst($category->name )}}
+                {{ ucfirst($category->name)}}
             </h3>
         </div>
     </div>
     <div class="px-3 pb-4">
-        <button class="primary-btn group inline-flex items-center justify-center gap-2 hover:bg-[#223a8f]">
+        <button class="primary-btn group/button inline-flex items-center justify-center gap-2 hover:bg-[#223a8f]">
             <span>Explore Now</span>
 
-            <span class="inline-flex transition-transform duration-300 ease-out group-hover:translate-x-1">
+            <span class="inline-flex transition-transform duration-300 ease-out group-hover/button:translate-x-1">
                 <i class="fa-solid fa-arrow-right text-sm"></i>
             </span>
         </button>
